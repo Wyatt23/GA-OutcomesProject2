9540327ad3cc84872119f8161413158a39ecbce5
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-20 18:21:35 -0400

    updated helpers for student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,40 @@
 class StudentsController < ApplicationController
+
+  def index
+    @students = Student.order(:last_name)
+  end
+
+  def new
+    @student = Student.new
+    # redirect to?
+  end
+
+  def create
+    @student = Student.create(student_params)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @artist.destroy
+    redirect_to students_path
+  end
+
+  private
+    def student_params
+      params.require (:student).permit(:first_name, :last_name, :ga_focus, :cohort_number)
+    end
+
 end
