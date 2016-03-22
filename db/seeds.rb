68f12f4166c516090e1281e7f3224ed73b420a35
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-22 10:13:57 -0400

    added styling and one brandseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,4 @@ Student.create(first_name: 'Caitlin', last_name: 'Daitch',ga_focus:'WDI', cohort
 Student.create(first_name: 'Cam', last_name: 'Latimer',ga_focus:'WDI', cohort_number:'8')
 
 
-# Brand.create(body:'An atypical breed of developer whose enthusiasm inspires a drive in team-members and clients alike. I am fearless in the face of ever-changing technology and avidly embrace the learning of new methodologies in my unique approach to problem-solving.', student_id:1)
+Brand.create(body:"I am a creative and energetic web developer with a knack for inspiring enthusiasm in my teammates. An adaptable and natural leader, I am not afraid to bring unconventional methods to the table in order to solve a problem and all the while,  I'll make sure everyone shares in the fun.", student_id:1)
